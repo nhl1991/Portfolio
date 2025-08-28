9339bb2ea06a5bd0886c7ee69d35dbd0bcc0d338
Laslark <laslark1991@gmail.com>
2025-08-28 16:55:41 +0900

    Add a project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -26,15 +26,7 @@ function CirclingNavigationWrapper() {
             <div className="w-full h-full flex overflow-hidden">
                 <Navigation />
                 <Description />
-                <div className="w-12 h-12 rounded-full  border-2 border-white hover:border-gray-600 fixed right-5 bottom-5 p-1 bg-gray-400">
-                    <div className="w-full h-full flex items-center justify-center p-2">
-                        <Link href={'/'}>
-                            <svg className="w-8 hover:stroke-gray-600" data-slot="icon" fill="none" strokeWidth={1.5} stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                            </svg>
-                        </Link>
-                    </div>
-                </div>
+                
             </div>
         </div>
     )
