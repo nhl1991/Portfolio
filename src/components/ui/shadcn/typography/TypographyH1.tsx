f25e99e09bb872d28d18ba649bc7eee718aa2d19
nhl1991 <laslark1991@gmail.com>
2026-02-20 00:13:56 +0900

    fix: Remove hard-coded id attribute

--- src/components/ui/shadcn/typography/TypographyH1.tsx
+++ src/components/ui/shadcn/typography/TypographyH1.tsx
@@ -1,7 +1,6 @@
 export function TypographyH1({children} : {children : React.ReactNode}) {
   return (
     <h1
-      id="intro_1"
       className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance"
     >
       {children}
